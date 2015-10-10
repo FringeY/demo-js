b9a77cbc51110c3aab54cf43edc6ba6bdee40ee2
yaorui <465964690@qq.com>
2015-10-10 21:43:26 +0800

    debug

---
+++ tools/tools.js
@@ -0,0 +1,14 @@
+var Tools = function () {
+    return Tools.prototype.init();
+};
+
+Tools.prototype = {
+    init: function () {
+        this.age = 13;
+        return this;
+    },
+    name: function () {
+
+    },
+    age: 20
+}
