bd88c0fb423389a83e6503f7f0e1fd91ef0da352
yaorui <465964690@qq.com>
2015-10-27 22:19:37 +0800

    fuckMooc

---
+++ fuckMooc/nopause.js
@@ -0,0 +1,74 @@
+var nopause = (function () {
+    // 递归查找是否存在blur
+    var findBlur = function (father) {
+        // 获取传入节点子节点数
+        var length = father.childNodes.length;
+
+        // 去除TEXT_NODE
+        if (father.nodeType == 3) {
+            return false;
+        }
+        
+        // 无子节点跳出循环
+        if (length == 0) {
+            return false;
+        }
+        
+        // 寻找blur属性
+        for (var i = 0; i < length; i++) {
+            var son = father.childNodes[i];
+            if (son.blur) {
+                clearBlur(son);
+                if (son.nodeName == 'FRAME') {
+                    findBlur(son.contentWindow.document);
+                } else {
+                    findBlur(son);
+                }
+            } 
+        }
+    }
+    // 清空blur
+    var clearBlur = function (obj) {
+        obj.blur = null;
+        console.log('rm ' + obj.nodeName + ' blur');
+        return true;
+    }
+
+    var test = function (father) {
+        // 获取传入节点子节点数
+        var length = father.childNodes.length;
+
+        // 去除TEXT_NODE
+        if (father.nodeType == 3) {
+            return false;
+        }
+        
+        // 无子节点跳出循环
+        if (length == 0) {
+            return false;
+        }
+        
+        // 寻找blur属性
+        for (var i = 0; i < length; i++) {
+            var son = father.childNodes[i];
+            console.log(1);
+            if (son.blur) {
+                console.log(son.nodeName);    
+            } 
+            if (son.nodeName == 'FRAME') {
+                test(son.contentWindow.document);
+            } else {
+                test(son);
+            }
+        }
+    }
+
+    return {
+        start: findBlur,
+        debug: test
+    };
+})();
+
+window.onload = function () {
+    nopause.start(document);
+}
